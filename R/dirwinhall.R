61fca7c6a646bda00a945b9cc09b036dd43a09c8
AlesiaT <Alesya.tretyakova@gmail.com>
2022-11-26 16:50:58 +0000

    Errors/warnings/notes eliminated

--- R/dirwinhall.R
+++ R/dirwinhall.R
@@ -1,5 +1,18 @@
+#' Irwin-Hall pdf
+#'
+#' @param x random variable, a positive real number <= n
+#' @param n number of independent random variables uniformly distributed from 0 to 1, n is a positive integer
+#'
+#' @return numeric value of probability density function for given x and n
+#' @export
+#'
+#' @seealso
+#'
+#' @examples
+#' dirwinhall(2.2, 5)
+#'
 dirwinhall <- function(x, n) {
-  if(!installr:::check.integer(n)) stop("n must be an integer")
+  if(!all.equal(n, as.integer(n))) stop("n must be an integer")
   if(n < 0) stop("n must be positive")
   if(x < 0) stop("x must be positive")
   if(x > n) stop("x must be less than or equal to n")
@@ -7,10 +20,3 @@ dirwinhall <- function(x, n) {
   sum((-1)^k * choose(n, k) * (x - k)^(n - 1))/factorial(n - 1)
 }
 
-# Tests
-dirwinhall(2, 5)
-dirwinhall(-2, 5)
-dirwinhall(22, 5)
-dirwinhall(2.2, 5)
-dirwinhall(2.2, 5.2)
-dirwinhall(2.2, -5)
