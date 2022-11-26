9e7aca507b0cfcec0c58b20fc7d1cf9508712ba4
AlesiaT <Alesya.tretyakova@gmail.com>
2022-11-26 18:40:04 +0000

    cdf function added

--- R/pirwinhall.R
+++ R/pirwinhall.R
@@ -1,23 +1,22 @@
-#' Irwin-Hall pdf
+#' Irwin-Hall cdf
 #'
 #' @param x random variable, a positive real number <= n
 #' @param n number of independent random variables uniformly distributed from 0 to 1, n is a positive integer
 #'
-#' @return numeric value of probability density function for given x and n
+#' @return numeric value of cumulative density function for given x and n
 #' @export
 #'
 #' @seealso
 #'
 #' @examples
-#' dirwinhall(2.2, 5)
+#' pirwinhall(2.2, 5)
 #'
-dirwinhall <- function(x, n) {
+pirwinhall <- function(x, n) {
   if(n != round(n)) stop("n must be an integer")
   if(n < 0) stop("n must be positive")
   if(x < 0) stop("x must be positive")
   if(x > n) stop("x must be less than or equal to n")
   k <- 0:abs(x)
-  sum((-1)^k * choose(n, k) * (x - k)^(n - 1))/factorial(n - 1)
+  sum((-1)^k * choose(n, k) * ((x - k)*(x > k))^n)/factorial(n)
 }
-
 
